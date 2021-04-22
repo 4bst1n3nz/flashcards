76a1e003c3574f3631e5d2ccdd1832df1e95967a
Sebastian <github@scherbes.de>
2021-04-22 15:25:44 +0200

    #6-7 Create and include topics slice

---
+++ src/features/topics/topicsSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const topicsSlice = createSlice({
+    name: 'topics',
+    initialState: {
+        topics: {}
+    },
+    reducers: {
+        addTopic(state, action) {
+            state.topics.topics[action.payload.id] = {
+                id: action.payload.id,
+                name: action.payload.name,
+                icon: action.payload.icon,
+                quizIds: []
+            };
+        }
+    }
+});
+
+export const { addTopic } = topicsSlice.actions;
+export default topicsSlice.reducer;
+
+// SELECTORS
+export const selectTopics = (state) => state.topics.topics;
